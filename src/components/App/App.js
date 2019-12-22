c91af3c7556e6191a8f763a05cd8907ee36139ff
Ben Stretar <ben.stretar@gmail.com>
2019-12-22 11:53:35 -0600

    added Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Details from '../Details/Details';
+import Edit from '../Edit/Edit';
+import MovieList from '../MovieList/MovieList';
 
 class App extends Component {
   // Renders the entire app on the DOM
@@ -7,9 +12,14 @@ class App extends Component {
     return (
       <div className="App">
         <p>Empty Page</p>
+        <Router>
+          <Route path='/' component={MovieList}/>
+          <Route path='/details' component={Details} />
+          <Route path='/edit' component={Edit}/>
+        </Router>
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App);
